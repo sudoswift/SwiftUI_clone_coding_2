84c02b5cc6b1083827f56a4b395094c705e1da50
sudoswift <sudoswift@gmail.com>
2021-05-21 01:53:55 +0900

    Initial Commit

---
+++ SwiftUI_clone_coding_2/SwiftUI_clone_coding_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_clone_coding_2App.swift
+//  SwiftUI_clone_coding_2
+//
+//  Created by izvern on 2021/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_clone_coding_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
